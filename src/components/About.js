74e68c8d1ebfb10e24822ce90328640ab91cb435
SHUBHAM SINGH <devbhoomi.shubhamsingh@gmail.com>
2023-01-08 13:01:13 +0530

    useLocation Hook Practice Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const About = () => {
+
+  const location = useLocation();
+  console.log("useLocation Hook Data:: ", location);
+
+  const {state} = location;
+  console.log('Location State Data: ', state)
+
+  /* OUTPUT */
+  /*
+  {
+    "pathname": "/aboutUs",
+    "search": "",
+    "hash": "",
+    "state": {
+        "name": "Shubham Singh",
+        "age": 27,
+        "mobileNo": "+91-9041934079",
+        "emailAddress": "devbhoomi.shubhamsingh@gmail.com",
+        "address": "246, Purvi Patel Nagar, Dehradun 248001, Uttarakhand"
+    },
+    "key": "7fpu5h6p"
+}
+  */
+
   return (
     <div>
       <h1>This is the About Us Page of our Application</h1>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,37 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const location = useLocation();
+  console.log("useLocation Hook Data:: ", location);
+  /* OUTPUT =
+  {
+    "pathname": "/",
+    "search": "",
+    "hash": "",
+    "state": null,
+    "key": "default"
+}
+  */
+
+  const [myProfile] = useState({
+    name: "Shubham Singh",
+    age: 27,
+    mobileNo: "+91-9041934079",
+    emailAddress: "devbhoomi.shubhamsingh@gmail.com",
+    address: "246, Purvi Patel Nagar, Dehradun 248001, Uttarakhand",
+  });
 
   const redirectToFilterPage = () => navigate("/filter");
 
   return (
     <div>
       <h1>This is the Home Page of our Application</h1>
-      <Link to={"/aboutUs"}>Go to About us Page</Link>
+      <Link to={"/aboutUs"} state={myProfile}>
+        Go to About us Page
+      </Link>
       <br />
       <br />
       <button onClick={() => navigate("/aboutUs")}>
